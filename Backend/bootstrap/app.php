afa0bfd826abc13254d3d4a5fa26bd00165cd952
Nextraile <nextraile@gmail.com>
2026-04-05 06:32:25 +0700

    refactor middleware and enhance error handling for API responses

--- Backend/bootstrap/app.php
+++ Backend/bootstrap/app.php
@@ -1,13 +1,17 @@
 <?php
 
+use App\Http\Helpers\ResponseHelper;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        // web: base_path('routes/web.php'),
         using: function () {
             Route::middleware('web')->group(base_path('routes/web.php'));
 
@@ -17,14 +21,23 @@ return Application::configure(basePath: dirname(__DIR__))
                 $versionPrefix = strtolower(basename(dirname($file)));
                 Route::middleware('api')->prefix("api/$versionPrefix")->group($file);
             }
+
+            Route::fallback(function () {
+                return response()->json([
+                    'response_code' => 404,
+                    'status' => 'error',
+                    'message' => 'API endpoint not found'
+                ], 404);
+            });
         },
+
         commands: __DIR__.'/../routes/console.php',
-        health: '/up',
+        health: '/api/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->api(prepend: [
-            \Illuminate\Http\Middleware\HandleCors::class,
             \App\Http\Middleware\ForceJsonResponse::class,
+            \Illuminate\Http\Middleware\HandleCors::class,
         ]);
 
         $middleware->api(append: [
@@ -33,4 +46,33 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-    })->create();
+        $exceptions->shouldRenderJsonWhen(function (Request $request, Throwable $e) {
+            return true;
+        })
+        ->render(function (Throwable $e, Request $request) {
+            
+            if ($e instanceof ValidationException) {
+                return ResponseHelper::errorResponse(
+                    message:'Validation failed',
+                    statusCode: Response::HTTP_UNPROCESSABLE_ENTITY,
+                    errors: $e->errors(),
+                    extra: [],
+                    exception: $e
+                );
+            }
+            if ($e instanceof HttpException) {
+                return ResponseHelper::errorResponse(
+                    message: $e->getMessage() ?: 'Unauthorized access or page not found',
+                    statusCode: $e->getStatusCode(),
+                    errors: [],
+                    extra: [],
+                    exception: $e
+                );
+            }
+            return ResponseHelper::internalServerErrorResponse(
+                exception: $e,
+                context: 'Internal Server Error',
+            );
+        });
+    })
+    ->create();
