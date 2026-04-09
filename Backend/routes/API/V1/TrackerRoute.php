0457bdeff1b579e4bd025ef03494eb49bd1190ad
Nextraile <nextraile@gmail.com>
2026-04-09 13:04:53 +0700

    refactor tracker routes: group tracker API resource under TrackerController for better organization

--- Backend/routes/API/V1/TrackerRoute.php
+++ Backend/routes/API/V1/TrackerRoute.php
@@ -3,4 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TrackerController;
 
-    Route::apiResource('trackers', TrackerController::class)->middleware('throttle:api');
+Route::controller(TrackerController::class)->group(function () {
+    Route::apiResource('trackers', TrackerController::class);
+});
